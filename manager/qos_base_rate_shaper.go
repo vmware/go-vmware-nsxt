ab70d927d7604ef71ee4b263b0bd51774e366d6b
Anna Khmelnitsky <akhmelnitsky@vmware.com>
2018-08-14 13:26:27 -0700

    Extend QosRateShaper to contain all attributes

--- manager/qos_base_rate_shaper.go
+++ manager/qos_base_rate_shaper.go
@@ -10,4 +10,13 @@ type QosBaseRateShaper struct {
 	Enabled bool `json:"enabled"`
 
 	ResourceType string `json:"resource_type,omitempty"`
+
+        // Average bandwidth in Mb/s
+        AverageBandwidthMbps int32 `json:"average_bandwidth_mbps,omitempty"`
+
+        // Burst size in bytes
+        BurstSizeBytes int32 `json:"burst_size_bytes,omitempty"`
+
+        // Average bandwidth in Mb/s
+        PeakBandwidthMbps int32 `json:"peak_bandwidth_mbps,omitempty"`
 }
